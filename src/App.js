b3a7b1c5312694d575b9afd5c127efba99c1e804
maria de los angeles <mariaramireeez2002@gmail.com>
2020-01-18 11:38:22 -0400

    pedir datos a la api digimon

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import Search from './components/Search'
 
 
 class App extends Component {
-  searchData=(termino)=>{
-    console.log(termino)
+  state={
+    term:'digimon'
+  }
+  queryApi=()=>{
+    const query=this.state.term
+    const url=`https://digimon-api.herokuapp.com/api/digimon/id/${query}`
+    fetch(url).then((res)=>res.json)
+    .then(data=>console.log(data))
+    //console.log(url)
+  }
+  searchData=(term)=>{
+    this.setState({term},()=>this.queryApi())
+
   }
   render(){
     return(
@@ -13,6 +24,9 @@ class App extends Component {
           <div className="jumbotron">
             <p className="lead text-center">Buscador de digimons</p>
             <Search  search={this.searchData}/>
+        
+
+
         </div>
       </div>
     );
