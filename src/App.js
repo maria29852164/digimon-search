dbff2a99e470ebe2a6eefe9ce889d741d7b8d981
maria de los angeles <mariaramireeez2002@gmail.com>
2020-01-18 10:32:48 -0400

    inicio del diseño

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+class App extends Component {
+  render(){
+    return(
+
+      <div className="app container">
+          <div className="jumbotron">
+            <p className="lead text-center">Buscador de digimons</p>
+        </div>
+      </div>
+    );
+
+  }
+}
+export default App
